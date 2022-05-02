4fceb78ccec0eb81a66f8b77e1c7a4c522f53ad7
Jofleming <jordano.fleming@gmail.com>
2022-05-01 21:43:28 -0700

    Lab 10 implementation

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,44 @@
 class Stack:
     """
-    Put docstring here
+    A linked list of values that resolves Last in First Out.
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        node = Node(value, self.top)
+        self.top = node
+
+    def pop(self):
+        if not self.top:
+            raise InvalidOperationError('Method not allowed on empty collection')
+        value_holder = self.top
+        self.top = self.top.next
+        return value_holder.value
+
+
+    def peek(self):
+        if not self.top:
+            raise InvalidOperationError('Method not allowed on empty collection')
+        else:
+            return self.top.value
+
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+
+    
+class Node:
+
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+class InvalidOperationError(Exception):
+    '''
+    Insert custom exception.
+    '''
+    pass
