10b3339ca15fdb5bb674a7e3ed3806b44da5532e
Jofleming <jordano.fleming@gmail.com>
2022-05-04 20:53:58 -0700

    finished challenge 13

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,31 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    checking_stack = Stack()
+
+    for char in string:
+        if (char == '(' or char == '{' or char == '['):
+           checking_stack.push(char) 
+        if char == ')':
+            if checking_stack.is_empty() == True:
+                return False
+            if checking_stack.peek() == '(':
+                checking_stack.pop()
+            else:
+                return False
+        if char == '}':
+            if checking_stack.is_empty() == True:
+                return False
+            if checking_stack.peek() == '{':
+                checking_stack.pop()
+            else:
+                return False
+        if char == ']':
+            if checking_stack.is_empty() == True:
+                return False
+            if checking_stack.peek() == '[':
+                checking_stack.pop()
+            else:
+                return False
+    return checking_stack.is_empty()
