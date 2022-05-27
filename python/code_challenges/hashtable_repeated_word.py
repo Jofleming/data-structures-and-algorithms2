64dcd901d3728aba14c043fcf49a566f121a8054
Jofleming <jordano.fleming@gmail.com>
2022-05-27 07:56:05 -0700

    finished challenge

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,16 @@
-from data_structures.hashtable import Hashtable
+# from data_structures.hashtable import Hashtable
+
+import re
 
 
-def first_repeated_word():
-    pass
+def first_repeated_word(word):
+    pattern = re.compile('[^a-zA-Z ]')
+    naked_content = pattern.sub('', word)
+    words_to_iterate = naked_content.lower().split()
+    temp_set = set()
+
+    for word in words_to_iterate:
+        if word in temp_set:
+            return word
+        else:
+            temp_set.add(word)
