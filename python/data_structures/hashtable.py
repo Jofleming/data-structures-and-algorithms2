46b66df66804e7819a70b359d4dcf44ee8ea7b25
Jofleming <jordano.fleming@gmail.com>
2022-05-31 08:44:24 -0700

    pushing up current work

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,60 @@
+
+
+from ctypes import sizeof
+
+from python.data_structures.linked_list import LinkedList
+
+
 class Hashtable:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, size=1024):
+        self.size = size
+        self.buckets = [None] * self.size
 
-    def some_method(self):
-        # method body here
-        pass
+
+    def hash(self, key):
+        char_sum = 0
+        for char in key:
+            char_sum += ord(char)
+        primed = char_sum * 599
+        index = primed % self.size
+        return index
+
+    def get(self, key):
+        index = self.hash(key)
+        bucket = self.buckets[index]
+        current = bucket.head
+
+        while current:
+            pair = current.value
+            current_key = pair[0]
+            if current_key == key:
+                return pair[1]
+            current = current.next
+        return None
+
+    def contains(self, key):
+        return bool(self.get(key))
+
+    def keys(self):
+        keys = set()
+        for bucket in self.buckets:
+            if bucket is not None:
+                current = bucket.head
+                while current:
+                    pair = current.value
+                    current_key = pair[0]
+                    keys.add(current_key)
+                    current = current.next
+        return keys
+
+    def set(self, key, value):
+        index = self.hash(key)
+        bucket = self.buckets[index]
+        if bucket is None:
+            self.buckets[index] = LinkedList()
+        
+        bucket.insert((key, value))
